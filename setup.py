ddea43a2220da0b6c72ab117f59eac0116de7946
Valentin Alexeev <valentin.alekseev@gmail.com>
2016-08-21 20:45:25 +0300

    Fix indentation.

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from distutils.core import setup
 
 setup(name='pcypair',
-      version='1.0',
-      description='Python scrapper for Cyprus Air Quality data',
-      author='Valentin Alexeev',
-      author_email='valentin.alekseev@gmail.com',
-      url='https://github.com/valentinalexeev/pcypair',
-      packages=['pcypair'],
-     )
+	version='1.0',
+	description='Python scrapper for Cyprus Air Quality data',
+	author='Valentin Alexeev',
+	author_email='valentin.alekseev@gmail.com',
+	url='https://github.com/valentinalexeev/pcypair',
+	packages=['pcypair'],
+	install_requires=['lxml']
+)
